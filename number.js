b35db3eb3dc8482bea296fe70661b118b9e31197
gaurav.rajput <gaurav.rajput@kochartech.com>
2022-04-17 13:13:00 +0530

    Chore: numbers and string

---
+++ number.js
@@ -0,0 +1,16 @@
+console.log(3 / 2);
+console.log(Math.floor(3 / 2));
+console.log(0.1 + 3.1);
+
+console.log(parseInt("123", 10));
+
+console.log(parseInt("010"));
+
+// ---------------NaN-------------//
+console.log("---------------NaN-------------");
+console.log("NaN + 5--->", NaN + 5);
+console.log("Number.isNaN(NaN)--->", Number.isNaN(NaN));
+console.log("isNaN.(5/0)--->", isNaN(5 / 0));
+
+console.log("------------isFinite------------");
+console.log("isFinite(1/0)----->", isFinite(1 / 0));
